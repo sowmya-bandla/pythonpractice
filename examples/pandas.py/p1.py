ad080bcbd1a4861df6de57b94e2de0fe328d9cbc
sowmya bandla <sowmyagayathri.bandla@gmail.com>
2025-05-27 16:59:52 -0400

    c

---
+++ examples/pandas.py/p1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+data=pd.Series([1,3,5])
+print(data)
+print(data.values)
+print(data.index)
+
+d1=pd.Series([1.25,4.5,6.7,3.9],index=['a','b','c','d'])
+print(d1)
+print(d1.values)
+print(d1.index)
+print('==============')
+d2=pd.Series({2:'a',1:'b',3:'c'},index=[3,1])
+print(d2)
+print('==============')
+
+population_dict={'cali':1234, 'texas':2345, 'ny':3456, 'florida':4567, 'illi':5678}
+area_dict={'cali':987, 'texas':876, 'ny':765, 'florida':654, 'illi':543}
+population=pd.Series(population_dict)
+area=pd.Series(area_dict)
+states=pd.DataFrame({'population':population, 'area':area})
+print(states)
+print(states.index)
+print(states.columns)
+print(pd.DataFrame(states,columns=['area']))
+print('==============')
+
+d=[{'a':10,'b':20},{'a':30,'c':40},{'d':50,'b':60}]
+df=pd.DataFrame(d)
+print(df)
+
