44d318ce2bbca9b820fb8cd3d832531fdd483b74
sowmya bandla <sowmyagayathri.bandla@gmail.com>
2025-06-02 16:45:35 -0400

    'c'

---
+++ examples/matplotlib.py/mat1.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+plt.plot([1,3,7,10])
+plt.ylabel('some numbers')
+plt.show() 
+
+plt.plot([1,2,3,4],[5,6,7,8],'ro')
+plt.axis([0,6,0,20])
+plt.show()
